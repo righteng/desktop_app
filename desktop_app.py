1fab2b79d487f445f0a7f33c863058251629ff0f
righteng <kazuto.ogoshi@gmail.com>
2024-10-28 23:42:58 +0900

    ウィンドウに「閉じるボタン」を追加。

--- desktop_app.py
+++ desktop_app.py
@@ -1,11 +1,32 @@
 import tkinter
 
+class Application(tkinter.Frame):
+    def __init__(self, root = None):
+        super().__init__(root,
+            width = 380, height = 280,
+            borderwidth = 1, relief = 'groove')
+        self.root = root
+        #表示位置
+        self.pack()
+        #サイズ調整
+        self.pack_propagate(0)
+        self.create_widgets()
+    
+    #閉じるボタン
+    def create_widgets(self):
+        quit_btn = tkinter.Button(self)
+        quit_btn['text'] = '閉じる'
+        quit_btn['command'] = self.root.destroy
+        quit_btn.pack(side = 'bottom')
+
 root = tkinter.Tk()
 
 #表示ウィンドウの名称
 root.title('test app')
 
 #表示ウィンドウの大きさ
-root.geometry('800x600')
+root.geometry('400x300')
 
-root.mainloop()
+app = Application(root = root)
+
+app.mainloop()
