a4133df645d01e1827094a9014418e50f7bbb76c
righteng <kazuto.ogoshi@gmail.com>
2024-10-29 23:08:32 +0900

    テキストボックスに入力した内容を出力する機能を追加。

--- desktop_app.py
+++ desktop_app.py
@@ -12,12 +12,31 @@ class Application(tkinter.Frame):
         self.pack_propagate(0)
         self.create_widgets()
     
-    #閉じるボタン
     def create_widgets(self):
+        # 閉じるボタン
         quit_btn = tkinter.Button(self)
         quit_btn['text'] = '閉じる'
         quit_btn['command'] = self.root.destroy
         quit_btn.pack(side = 'bottom')
+
+        # テキストボックス
+        self.text_box = tkinter.Entry(self)
+        self.text_box['width'] = 10
+        self.text_box.pack()
+
+        # 実行ボタン
+        submit_btn = tkinter.Button(self)
+        submit_btn['text'] = '実行'
+        submit_btn['command'] = self.input_handler
+        submit_btn.pack()
+
+        # メッセージ出力
+        self.message = tkinter.Message(self)
+        self.message.pack()
+
+    def input_handler(self):
+        text = self.text_box.get()
+        self.message['text'] = text + '!!'
 
 root = tkinter.Tk()
 
